85994e8b4683e0187b2f536e1b65bebb6c2ecaf0
tongzhou <tongzhou>
2016-04-07 16:28:54 +0800

    Initial Commit

---
+++ JiaJia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JiaJia
+//
+//  Created by jojoting on 16/4/7.
+//  Copyright © 2016年 jojoting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
